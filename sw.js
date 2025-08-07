f643c812f027459d249d59cdc3c4ae5334ab16db
Kallenavn <kallenavn@192.168.1.106>
2025-08-08 01:21:56 +0200

    Initial commit: IDR Calc v6

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'idr-calc-v1.0.3';
+const OFFLINE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon.svg',
+  'https://cdn.tailwindcss.com',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(OFFLINE_ASSETS);
+    self.skipWaiting();
+  })());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    const cached = await cache.match(req);
+    if (cached) return cached;
+    try {
+      const fresh = await fetch(req);
+      if (req.method === 'GET' && new URL(req.url).origin === location.origin) {
+        cache.put(req, fresh.clone());
+      }
+      return fresh;
+    } catch (err) {
+      if (req.mode === 'navigate') return cache.match('./index.html');
+      throw err;
+    }
+  })());
+});
